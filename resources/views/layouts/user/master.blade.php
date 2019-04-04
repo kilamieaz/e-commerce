9e5ab9ddc6b41714190327e13e0ec615433f8a75
Fajar Jr <fajarjunyor13@gmail.com>
2019-04-04 16:57:24 +0700

    updatefav icon

--- resources/views/layouts/user/master.blade.php
+++ resources/views/layouts/user/master.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="format-detection" content="telephone=no">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <!-- <link href="apple-touch-icon.png') }}" rel="apple-touch-icon"> -->
-    <!-- <link href="" rel="icon"> -->
+    <link href="{{ asset('apple-touch-icon.png') }}" rel="apple-touch-icon">
+    <link href="{{ asset('favicon-thecloth.png') }}" rel="icon">
     <meta name="author" content="">
     <meta name="keywords" content="Default Description">
     <meta name="description" content="Default keyword">
