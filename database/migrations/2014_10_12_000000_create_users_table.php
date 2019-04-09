76b4e1863ad7efb6ae152513778968b9edddb3ec
kilamieaz <im.sultaan05@gmail.com>
2019-04-09 13:39:40 +0700

    repair migration user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedBigInteger('role_id')->index();
+            $table->unsignedBigInteger('role_id')->index()->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
