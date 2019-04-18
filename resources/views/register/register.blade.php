acfdc2a01335873ac33eda44cdfa616974217ba6
Fajar Jr <fajarjunyor13@gmail.com>
2019-04-18 16:48:16 +0700

    add validation and some bug :D

--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -3,6 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Please SignUp</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
     <link rel="stylesheet" href="/css/auth.css">
@@ -19,35 +20,39 @@
     <li>Personal Details</li>
   </ul>
   <!-- fieldsets -->
-  <fieldset>
+  <fieldset class="fieldset1">
     <h2 class="fs-title">Create your account</h2>
     <h3 class="fs-subtitle">This is step 1</h3>
-    <input type="text" name="email" placeholder="Email" />
-    <input type="password" name="pass" placeholder="Password" />
-    <input type="password" name="cpass" placeholder="Confirm Password" />
-    <input type="button" name="next" class="next action-button" value="Next" />
+    <input type="text" name="email" placeholder="Email" required/>
+    <input id="password" name="password" type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Must have at least 6 characters' : ''); if(this.checkValidity()) form.password_two.pattern = this.value;" placeholder="Password" required>
+    <input id="password_two" name="password_two" type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" onchange="this.setCustomValidity(this.validity.patternMismatch ? 'Please enter the same Password as above' : '');" placeholder="Verify Password" required>
+    <input type="submit" id="submitBtn" name="next" class="next action-button" value="Next" />
     <a id="forgot" href="signin" style="text-decoration:none;">
                 <h3 class="fs-subtitle">Already Have an Account?</h3>
             </a>
+    <script>
+
+
+    </script>
   </fieldset>
-  <fieldset>
+  <fieldset class="fieldset2">
     <h2 class="fs-title">Social Profiles</h2>
     <h3 class="fs-subtitle">Your presence on the social network</h3>
-    <input type="text" name="twitter" placeholder="Twitter" />
-    <input type="text" name="facebook" placeholder="Facebook" />
-    <input type="text" name="gplus" placeholder="Google Plus" />
+    <input type="text" name="twitter" placeholder="Twitter" required/>
+    <input type="text" name="facebook" placeholder="Facebook" required/>
+    <input type="text" name="gplus" placeholder="Google Plus"required />
     <input type="button" name="previous" class="previous action-button" value="Previous" />
-    <input type="button" name="next" class="next action-button" value="Next" />
+    <input type="submit" id="submitBtn2" name="next" class="next2 action-button" value="Next" />
   </fieldset>
-  <fieldset>
+  <fieldset class="fieldset3">
     <h2 class="fs-title">Personal Details</h2>
     <h3 class="fs-subtitle">We will never sell it</h3>
-    <input type="text" name="fname" placeholder="First Name" />
-    <input type="text" name="lname" placeholder="Last Name" />
-    <input type="text" name="phone" placeholder="Phone" />
+    <input type="text" name="fname" placeholder="First Name" required/>
+    <input type="text" name="lname" placeholder="Last Name" required/>
+    <input type="text" name="phone" placeholder="Phone" required/>
     <textarea name="address" placeholder="Address"></textarea>
-    <input type="button" name="previous" class="previous action-button" value="Previous" />
-    <input type="submit" name="submit" class="submit action-button" value="Submit" />
+    <input type="submit"  name="previous" class="previous action-button" value="Previous" />
+    <input type="submit" id="submit" name="submit" class="submit action-button" value="Submit" />
   </fieldset>
 </form>
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
