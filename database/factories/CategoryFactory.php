003176f1d68c60b857a303c5a3d0af252d3eeffa
kilamieaz <im.sultaan05@gmail.com>
2019-04-28 15:16:05 +0700

    repair category factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,7 +5,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
     return [
-        'name' => $faker->lexify('Parent???'),
+        'name' => $faker->lexify('parent???'),
         'description' => $faker->lexify('Description Category ???'),
         'image' => $faker->image('public/storage/files', 400, 300, null, false),
         'status' => 1,
